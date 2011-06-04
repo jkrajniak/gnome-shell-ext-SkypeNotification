1e2a30241f8223686d8dd3d5a0996f16d8822b91
Jakub Krajniak <jkrajniak@gmail.com>
2011-06-04 23:36:53 +0200

    - licence, issue #1

--- SkypeNotification@krajniak.info/extension.js
+++ SkypeNotification@krajniak.info/extension.js
@@ -1,9 +1,26 @@
 /*
+ * gnome-shell-extension-SkypeNotification
  * Skype GnomeShell Integration.
+ *  
+ * This file is part of gnome-shell-extension-SkypeNotification.
+ *
+ * gnome-shell-ext-SkypeNotification is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gnome-shell-ext-SkypeNotification  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gnome-shell-ext-SkypeNotification  If not, see <http://www.gnu.org/licenses/>.
  *
  * Credits to the author of Gajim extension as this extension code was modified
- * from it and author of Skype extension
+ * from it.
  * 
+ * Copyright (C) 2011
  * Author: Jakub Krajniak <jkrajniak@gmail.com>
  * 
  */
